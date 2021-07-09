ab47bbf043333cfc3ce548d133606bb5f6529f3a
Ito Hiroki <hiroki.i0307@gmail.com>
2021-07-09 20:02:23 +0900

    Add raindrops

---
+++ raindrops/src/lib.rs
@@ -0,0 +1,17 @@
+pub fn raindrops(n: u32) -> String {
+    let mut answer = String::new();
+    if n % 3 == 0 {
+        answer.push_str("Pling");
+    }
+    if n % 5 == 0 {
+        answer.push_str("Plang");
+    }
+    if n % 7 == 0 {
+        answer.push_str("Plong");
+    }
+    if answer == "" {
+        n.to_string()
+    } else {
+        answer
+    }
+}
